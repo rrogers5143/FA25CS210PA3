a3b457d28c0fdaae7836ff049dda5d07ecf3d642
Rachel Rogers <rrogers5143@sdsu.edu>
2025-12-06 22:50:19 -0800

    Had to make some (non-logic altering) syntax changes throughout the main.cpp to get it to run because I am running an older version of c++. Hope that's ok.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int dc[4] = {0, 1, 0, -1};
 // ----------------------------------------------------------
 // DO NOT MODIFY: Maze generation
 // ----------------------------------------------------------
-void generateMaze(vector<vector<int>>& maze, int N, int M) {
+void generateMaze(vector<vector<int> >& maze, int N, int M) {
     srand(time(0));
 
     for (int r = 0; r < N; r++) {
@@ -30,7 +30,7 @@ void generateMaze(vector<vector<int>>& maze, int N, int M) {
 // ----------------------------------------------------------
 // DO NOT MODIFY: Choose a random boundary cell that is open
 // ----------------------------------------------------------
-pair<int,int> chooseBoundaryCell(const vector<vector<int>>& maze) {
+pair<int,int> chooseBoundaryCell(const vector<vector<int> >& maze) {
     int N = maze.size();
     int M = maze[0].size();
 
@@ -53,7 +53,7 @@ pair<int,int> chooseBoundaryCell(const vector<vector<int>>& maze) {
         }
 
         if (maze[r][c] == 0) {
-            return {r, c};
+            return make_pair(r, c);
         }
     }
 }
@@ -61,7 +61,7 @@ pair<int,int> chooseBoundaryCell(const vector<vector<int>>& maze) {
 // ----------------------------------------------------------
 // DO NOT MODIFY: Print the maze with S and E markers
 // ----------------------------------------------------------
-void printMaze(const vector<vector<int>>& maze,
+void printMaze(const vector<vector<int> >& maze,
                int ent_r, int ent_c,
                int exit_r, int exit_c)
 {
@@ -88,24 +88,24 @@ void printMaze(const vector<vector<int>>& maze,
 // Students must fill parent[][] correctly during DFS
 // ----------------------------------------------------------
 void printPath(pair<int,int> exitcell,
-               const vector<vector<int>>& parent_r,
-               const vector<vector<int>>& parent_c,
+               const vector<vector<int> >& parent_r,
+               const vector<vector<int> >& parent_c,
                int ent_r, int ent_c)
 {
     int r = exitcell.first;
     int c = exitcell.second;
 
-    vector<pair<int,int>> path;
+    vector<pair<int,int> > path;
 
     // Walk backward from exit to entrance
     while (!(r == ent_r && c == ent_c)) {
-        path.push_back({r, c});
+        path.push_back( make_pair(r, c) );
         int pr = parent_r[r][c];
         int pc = parent_c[r][c];
         r = pr;
         c = pc;
     }
-    path.push_back({ent_r, ent_c});
+    path.push_back( make_pair(ent_r, ent_c) );
 
     cout << "\nPath from entrance to exit:\n";
     for (int i = path.size() - 1; i >= 0; i--) {
@@ -135,7 +135,7 @@ int main() {
     cout << "Enter maze dimensions N M: ";
     cin >> N >> M;
 
-    vector<vector<int>> maze(N, vector<int>(M));
+    vector<vector<int> > maze(N, vector<int>(M));
     generateMaze(maze, N, M);
 
     // Pick entrance and exit
@@ -155,9 +155,9 @@ int main() {
     printMaze(maze, ent_r, ent_c, exit_r, exit_c);
 
     // Students must use these
-    vector<vector<bool>> visited(N, vector<bool>(M, false));
-    vector<vector<int>> parent_r(N, vector<int>(M, -1));
-    vector<vector<int>> parent_c(N, vector<int>(M, -1));
+    vector<vector<bool> > visited(N, vector<bool>(M, false));
+    vector<vector<int> > parent_r(N, vector<int>(M, -1));
+    vector<vector<int> > parent_c(N, vector<int>(M, -1));
 
     // ------------------------------------------------------
     // STUDENT WORK:
